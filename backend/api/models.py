65ddae0d6048f7d95738c257587899b76645acf9
unknown <emmanueladesipe01@gmail.com>
2025-05-06 22:27:38 -0700

    completed

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Todo(models.Model):
     text = models.CharField(max_length=255)
-    updated = models.BooleanField(default=False)
+    completed = models.BooleanField(default=False)
     def __str__(self):
         return self.text
